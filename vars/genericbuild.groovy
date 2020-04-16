2d1f128cc5fb61077f4bb13cc759d5b7e653f06c
Taylor Herald <t.herald7@hotmail.com>
2020-04-16 12:18:08 -0500

    initial commit

---
+++ vars/genericbuild.groovy
@@ -0,0 +1,30 @@
+def call(Map config=[:]) {
+    node {
+        stage('SCM') {
+            echo 'Gathering code from version control....'
+            git branch: '${branch}', url: 'https://github.com/therald/JenkinsfileDemo.git'
+        }
+        stage('Build') {
+            try {
+                echo 'Building....'
+                sh 'dotnet build ConsoleApp1'
+                releasenotes(changes:"true")
+            }
+            catch(ex) {
+                echo 'Something went wrong'
+                echo ex.toString()
+                currentBuild.result = 'FAILURE'
+            }
+            finally {
+                // cleanup
+            }
+            
+        }
+        stage('Test') {
+            echo 'Testing....'
+        }
+        stage('Deploy') {
+            echo 'Deploying....'
+        }
+    }
+}
